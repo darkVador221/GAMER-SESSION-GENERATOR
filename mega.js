2584acbdcdc9a1655c9c61627bd37435f1810037
darkVador221 <vador2899@gmail.com>
2025-07-10 23:30:32 +0000

    Create mega.js

---
+++ mega.js
@@ -0,0 +1,32 @@
+const mega = require('megajs');
+
+const credentials = {
+  email: 'wohabo1681@calorpg.com',
+  password: 'Chamindu2008',
+  userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
+};
+
+const upload = (stream, filename) => {
+  return new Promise((resolve, reject) => {
+    try {
+      const storage = new mega.Storage(credentials, () => {
+        const options = {
+          name: filename,
+          allowUploadBuffering: true
+        };
+        stream.pipe(storage.upload(options));
+        storage.on('add', file => {
+          file.link((err, link) => {
+            if (err) return reject(err);
+            storage.close();
+            resolve(link);
+          });
+        });
+      });
+    } catch (err) {
+      reject(err);
+    }
+  });
+};
+
+module.exports = { upload };
