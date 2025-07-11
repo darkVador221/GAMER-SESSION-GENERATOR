ee7e15b20b46de37408cd27d3e93d9f35d109990
darkVador221 <vador2899@gmail.com>
2025-07-11 16:23:52 +0000

    Update index.js

--- index.js
+++ index.js
@@ -1,20 +1,37 @@
 const express = require('express');
 const app = express();
-const path = require('path');
-const bodyParser = require("body-parser");
+console.log('🚀 GAMER XMD Server starting...');
 
+__path = process.cwd();
+const bodyParser = require("body-parser");
 const PORT = process.env.PORT || 8000;
 
-let qr = require('./qr');
-let pair = require('./pair');
+let server = require('./qr'),
+    code = require('./pair');
 
-app.use('/qr', qr);
-app.use('/pair', pair);
-app.use('/', (req, res) => res.sendFile(path.join(__dirname, 'main.html')));
+require('events').EventEmitter.defaultMaxListeners = 500;
 
+// Routes
+app.use('/server', server);
+app.use('/code', code);
+
+// Nouvelle route pair.html (formulaire)
+app.get('/pair-form', (req, res) => {
+  res.sendFile(__path + '/pair.html');
+});
+
+app.use('/pair', code);
+app.use('/qr', server);
+app.use('/', (req, res) => {
+  res.sendFile(__path + '/main.html');
+});
+
+// Body Parser
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.listen(PORT, () => {
-  console.log("✅ GAMER XMD Session Manager running on port", PORT);
+  console.log(`🟢 GAMER XMD running on http://localhost:${PORT}`);
 });
+
+module.exports = app;
