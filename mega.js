e50c5fec82dbd2de83e09ad8339a1d06a9c6a5f4
darkVador221 <vador2899@gmail.com>
2025-07-11 01:42:09 +0000

    Update mega.js

--- mega.js
+++ mega.js
@@ -3,23 +3,24 @@ const mega = require('megajs');
 const credentials = {
   email: 'wohabo1681@calorpg.com',
   password: 'Chamindu2008',
-  userAgent: 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
+  userAgent:
+    'Mozilla/5.0 (Windows NT 10.0; Win64; x64) ' +
+    'AppleWebKit/537.36 (KHTML, like Gecko) ' +
+    'Chrome/42.0.2311.135 Safari/537.36 Edge/12.246'
 };
 
 const upload = (stream, filename) => {
   return new Promise((resolve, reject) => {
     try {
       const storage = new mega.Storage(credentials, () => {
-        const options = {
-          name: filename,
-          allowUploadBuffering: true
-        };
-        stream.pipe(storage.upload(options));
+        const file = storage.upload({ name: filename, allowUploadBuffering: true });
+        stream.pipe(file);
+
         storage.on('add', file => {
-          file.link((err, link) => {
+          file.link((err, url) => {
             if (err) return reject(err);
             storage.close();
-            resolve(link);
+            resolve(url);
           });
         });
       });
