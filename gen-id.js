299042676923f065ddee006a9b6634e95c504c8f
darkVador221 <vador2899@gmail.com>
2025-07-12 21:19:17 +0000

    Update gen-id.js

--- gen-id.js
+++ gen-id.js
@@ -1,10 +1,12 @@
-function makeid(num = 4) {
-  let result = "";
-  let characters = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
-  var charactersLength = characters.length;
-  for (var i = 0; i < num; i++) {
+// gen-id.js
+function makeid(length = 5) {
+  let result = '';
+  const characters = 'ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789';
+  const charactersLength = characters.length;
+  for (let i = 0; i < length; i++) {
     result += characters.charAt(Math.floor(Math.random() * charactersLength));
   }
   return result;
 }
+
 module.exports = { makeid };
