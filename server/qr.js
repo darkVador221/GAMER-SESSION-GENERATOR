c321dc27162e5156a09413a82598562b49a304e5
darkVador221 <vador2899@gmail.com>
2025-07-12 01:39:14 +0000

    Update qr.js

--- server/qr.js
+++ server/qr.js
@@ -3,6 +3,7 @@ const router = express.Router();
 const { Client, RemoteAuth } = require('whatsapp-web.js');
 const SessionManager = require('../core/sessionManager');
 const path = require('path');
+const QRCode = require('qrcode');
 
 router.get('/', (req, res) => {
     res.sendFile(path.join(__dirname, '../public/qr.html'));
@@ -10,15 +11,34 @@ router.get('/', (req, res) => {
 
 router.get('/api/qr', async (req, res) => {
     const client = new Client({
-        puppeteer: { headless: true },
+        puppeteer: { headless: true, args: ['--no-sandbox', '--disable-setuid-sandbox'] },
         authStrategy: new RemoteAuth({
             store: SessionManager,
             backupSyncIntervalMs: 300000
         })
     });
 
-    client.once('qr', (qr) => {
-        return res.json({ qr });
+    let responded = false;
+
+    client.on('qr', async (qr) => {
+        if (!responded) {
+            responded = true;
+            res.status(200).json({ qr });
+        }
+    });
+
+    client.on('auth_failure', () => {
+        if (!responded) {
+            responded = true;
+            res.status(401).json({ error: 'QR Auth Failure' });
+        }
+    });
+
+    client.on('ready', () => {
+        if (!responded) {
+            responded = true;
+            res.status(400).json({ error: 'Déjà connecté' });
+        }
     });
 
     client.initialize();
